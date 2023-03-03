c65d25501d3e837c9f35867658a08461b084ad93
Li MA <maleewahaha@gmail.com>
2023-03-03 17:42:44 +0800

    Public Release version 0.1 Add merge_neighbor_pixels when exporting the mesh

--- script_export_mesh.py
+++ script_export_mesh.py
@@ -4,17 +4,79 @@ import numpy as np
 import imageio
 import json
 from dataloader import load_llff_data
+from config_parser import config_parser
 from utils import save_obj_with_vcolor, save_obj_multimaterial, normalize_uv, cull_unused
 
+PATCH_SIZE = 16
 
-def export_mpv_repr(cfg_file, cfg_file1):
-    from config_parser import config_parser
-    parser = config_parser()
-    args = parser.parse_args(["--config", cfg_file, "--config1", cfg_file1])
 
+# merge the neighbor pixels to prevent tiling artifact
+# failed experiment, turns out this will leads to artifact
+# so this function is currently unused
+def merge_neighbor_pixels(v, f, uv, uvf, atlas):  # len(f) == len(uvf)
+    # faces of a quad: 0 - 1   two face is arraged as (0, 1, 3), (3, 2, 0)
+    #                  | \ |
+    #                  2 - 3
+    h, w, _ = atlas.shape
+    f = f.reshape(-1, 6)
+    uvf = uvf.reshape(-1, 6)
+
+    # find neighbor edge (horizon
+    edge_h = f[:, [0, 4, 1, 2]].reshape(-1, 2)
+    edge_h_uv = uvf[:, [0, 4, 1, 2]].reshape(-1, 2)
+    edge_h_flat = edge_h[:, 0] + edge_h[:, 1] * len(v)
+    sortidx = np.argsort(edge_h_flat)
+    edge_h_flat = edge_h_flat[sortidx]
+    edge_h_uv = edge_h_uv[sortidx]
+    _, idx, counts = np.unique(edge_h_flat, return_index=True, return_counts=True)
+    idx0 = idx[np.argwhere(counts == 2)][:, 0]
+    idx1 = idx0 + 1
+
+    uv_select = uv[edge_h_uv[idx0]]
+    uv_select1 = uv[edge_h_uv[idx1]]
+    # | |
+    x_idx0, x_idx1, y_idx0, y_idx1 = uv_select[:, 0, 0], uv_select1[:, 0, 0], uv_select[:, 0, 1], uv_select1[:, 0, 1]
+    x_idx0 = np.round((x_idx0 + 1) / (2 / (w - 1))).astype(np.int32)
+    x_idx1 = np.round((x_idx1 + 1) / (2 / (w - 1))).astype(np.int32)
+    y_idx0 = np.round((y_idx0 + 1) / (2 / (h - 1))).astype(np.int32)
+    y_idx1 = np.round((y_idx1 + 1) / (2 / (h - 1))).astype(np.int32)
+    rang = np.arange(PATCH_SIZE)
+    pix_loc0 = [np.stack([x_idx0[:, None].repeat(len(rang), 1), y_idx0[:, None] + rang[None]], axis=-1)]
+    pix_loc1 = [np.stack([x_idx1[:, None].repeat(len(rang), 1), y_idx1[:, None] + rang[None]], axis=-1)]
+
+    # find neighbor edge (vertical)
+    edge_v = f[:, [0, 1, 4, 2]].reshape(-1, 2)
+    edge_v_uv = uvf[:, [0, 1, 4, 2]].reshape(-1, 2)
+    edge_v_flat = edge_v[:, 0] + edge_v[:, 1] * len(v)
+    sortidx = np.argsort(edge_v_flat)
+    edge_v_flat = edge_v_flat[sortidx]
+    edge_v_uv = edge_v_uv[sortidx]
+    _, idx, counts = np.unique(edge_v_flat, return_index=True, return_counts=True)
+    idx0 = idx[np.argwhere(counts == 2)][:, 0]
+    idx1 = idx0 + 1
+
+    uv_select = uv[edge_v_uv[idx0]]
+    uv_select1 = uv[edge_v_uv[idx1]]
+    # --
+    # --
+    x_idx0, x_idx1, y_idx0, y_idx1 = uv_select[:, 0, 0], uv_select1[:, 0, 0], uv_select[:, 0, 1], uv_select1[:, 0, 1]
+    x_idx0 = np.round((x_idx0 + 1) / (2 / (w - 1))).astype(np.int32)
+    x_idx1 = np.round((x_idx1 + 1) / (2 / (w - 1))).astype(np.int32)
+    y_idx0 = np.round((y_idx0 + 1) / (2 / (h - 1))).astype(np.int32)
+    y_idx1 = np.round((y_idx1 + 1) / (2 / (h - 1))).astype(np.int32)
+    rang = np.arange(PATCH_SIZE)
+    pix_loc0 += [np.stack([x_idx0[:, None] + rang[None], y_idx0[:, None].repeat(len(rang), 1)], axis=-1)]
+    pix_loc1 += [np.stack([x_idx1[:, None] + rang[None], y_idx1[:, None].repeat(len(rang), 1)], axis=-1)]
+
+    pix1 = np.concatenate(pix_loc0).reshape(-1, 2)
+    pix2 = np.concatenate(pix_loc1).reshape(-1, 2)
+    return pix1, pix2
+
+
+def export_mpv_repr(args):
     prefix = args.prefix
     expname = args.expname + args.expname_postfix
-    outpath = os.path.join(prefix, "mesh4demo", expname)
+    outpath = os.path.join(prefix, args.mesh_folder, expname)
     os.makedirs(outpath, exist_ok=True)
 
     data_dir = os.path.join(prefix, args.datadir)
@@ -78,6 +140,18 @@ def export_mpv_repr(cfg_file, cfg_file1):
     assert frame_num == args.mpv_frm_num, "Error: detect unmatched frame count"
     # saving geometry
 
+    # pix1, pix2 = merge_neighbor_pixels(verts, faces_static, uvs_static, uvfaces_static, atlas_static)
+    # color1, color2 = atlas_static[pix1[:, 1], pix1[:, 0]], atlas_static[pix2[:, 1], pix2[:, 0]]
+    # color = np.minimum(color1, color2)
+    # atlas_static[pix1[:, 1], pix1[:, 0]] = color
+    # atlas_static[pix2[:, 1], pix2[:, 0]] = color
+    # # will chagne atlas_dynamic
+    # pix1, pix2 = merge_neighbor_pixels(verts, faces_dynamic, uvs_dynamic, uvfaces_dynamic, atlas_dynamic[0])
+    # color1, color2 = atlas_dynamic[:, pix1[:, 1], pix1[:, 0]], atlas_dynamic[:, pix2[:, 1], pix2[:, 0]]
+    # color = np.minimum(color1, color2)
+    # atlas_dynamic[:, pix1[:, 1], pix1[:, 0]] = color
+    # atlas_dynamic[:, pix2[:, 1], pix2[:, 0]] = color
+
     uvs_static = normalize_uv(uvs_static, atlas_h_static, atlas_w_static)
     uvs_dynamic = normalize_uv(uvs_dynamic, atlas_h_dynamic, atlas_w_dynamic)
 
@@ -118,29 +192,8 @@ def export_mpv_repr(cfg_file, cfg_file1):
 
 
 if __name__ == "__main__":
-    cfg1s = [
-        # "108fall1",
-        # "108fall2",
-        # "108fall3",
-        "108fall4",
-        # "108fall5",
-        # "110grasstree",
-        # "110pillar",
-        # "1017palm",
-        # "1017yuan",
-        # "1020rock",
-        # "1020ustfall1",
-        # "1020ustfall2",
-        # "1101grass",
-        # "1101towerd",
-        # "ustfallclose",
-        # "usttap"
-    ]
-
-    for cfg1 in cfg1s:
-        export_mpv_repr(
-            "configs/mpv_base.txt",
-            f"configs/mpvs/{cfg1}.txt",
-        )
-
-
+    parser = config_parser()
+    parser.add_argument("--mesh_folder", type=str, default="postrebuttal_mesh",
+                        help='')
+    args = parser.parse_args()
+    export_mpv_repr(args)
